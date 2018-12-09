7d51cfed6fcab0e9168672922747f32f21c3384a
Jaewan Park <jaewan.huey.park@gmail.com>
2018-12-09 23:05:13 +0900

    Create simple tcp server

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println(err)
+		}
+
+		log.Println(conn)
+	}
+}

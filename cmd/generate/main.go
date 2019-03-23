b502e481dd4335ba91997152c0295809dc3b5223
Jaewan Park <jaewan.huey.park@gmail.com>
2019-03-24 01:17:31 +0900

    Create generateAsset command

--- cmd/generate/main.go
+++ cmd/generate/main.go
@@ -5,10 +5,12 @@ import (
 	"log"
 	"os/exec"
 	"path/filepath"
+	"strings"
 )
 
 func main() {
 	generateFlatbuffers()
+	generateAsset()
 }
 
 func generateFlatbuffers() {
@@ -19,6 +21,32 @@ func generateFlatbuffers() {
 
 	for _, f := range files {
 		cmd := exec.Command("flatc", "--go", "-o", "./game/message", f)
+		cmdOutput := &bytes.Buffer{}
+		cmd.Stdout = cmdOutput
+		err := cmd.Run()
+		if err != nil {
+			log.Println(cmdOutput.String())
+			log.Fatalln(err)
+		}
+	}
+}
+
+func generateAsset() {
+	files, err := filepath.Glob("./client/asset/*.png")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	for _, f := range files {
+		name := strings.Replace(filepath.Base(f), ".png", "", 1)
+
+		cmd := exec.Command(
+			"file2byteslice",
+			"-input", f,
+			"-output", strings.Replace(f, ".png", ".go", 1),
+			"-package", "asset",
+			"-var", strings.Title(name))
+
 		cmdOutput := &bytes.Buffer{}
 		cmd.Stdout = cmdOutput
 		err := cmd.Run()

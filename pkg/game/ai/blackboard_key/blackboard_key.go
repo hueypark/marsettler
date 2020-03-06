02a95ee4ccca7c6ba9535a87499ae7b2309eaab4
Jaewan Park <jaewan.huey.park@gmail.com>
2020-03-06 09:13:00 +0900

    Delete unused code

--- pkg/game/ai/blackboard_key/blackboard_key.go
+++
@@ -1,7 +0,0 @@
-package blackboard_key
-
-import "github.com/hueypark/marsettler/core/behavior_tree"
-
-const (
-	Path behavior_tree.BlackboardKey = iota
-)
--- pkg/game/ai/task/actor.go
+++
@@ -1,7 +0,0 @@
-package task
-
-type Actor interface {
-	CreateActor(id int)
-	FindPath() *[]int64
-	Move(nodeID int64)
-}

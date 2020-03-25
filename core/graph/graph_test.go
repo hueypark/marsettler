e122d7b465a2598031ef6a10093179ffd1fb6931
Jaewan Park <jaewan.huey.park@gmail.com>
2020-03-25 22:53:41 +0900

    Add some real benchmark

--- core/graph/graph_test.go
+++ core/graph/graph_test.go
@@ -1,7 +1,44 @@
 package graph
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/hueypark/marsettler/core/math/vector"
+)
+
+type benchmarkNode struct {
+	id  int64
+	pos vector.Vector
+}
+
+func (n *benchmarkNode) ID() int64 {
+	return n.id
+}
+
+func (n *benchmarkNode) Position() vector.Vector {
+	return n.pos
+}
+
+func newNode(id int64, pos vector.Vector) *benchmarkNode {
+	return &benchmarkNode{
+		id:  id,
+		pos: pos,
+	}
+}
 
 func BenchmarkPath(b *testing.B) {
+	startNode := newNode(1, vector.Vector{X: 0, Y: 0})
+	endNode := newNode(2, vector.Vector{X: 0, Y: 0})
+	g := NewGraph()
+	g.AddNode(startNode)
+	g.AddNode(endNode)
+	g.AddEdge(startNode.ID(), endNode.ID())
 
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, err := g.Path(startNode.ID(), endNode.ID())
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
 }

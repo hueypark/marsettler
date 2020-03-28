d67103b2ace31d671d2df63bafd7451aab58fa96
Jaewan Park <jaewan.huey.park@gmail.com>
2020-03-28 18:43:22 +0900

    Create BenchmarkRotateVector

--- core/math/rotator/rotator_test.go
+++ core/math/rotator/rotator_test.go
@@ -1,6 +1,11 @@
 package rotator
 
-import "testing"
+import (
+	"math"
+	"testing"
+
+	"github.com/hueypark/marsettler/core/math/vector"
+)
 
 func TestRotateVector(t *testing.T) {
 	//a := assert.New(t)
@@ -12,4 +17,14 @@ func TestRotateVector(t *testing.T) {
 	// TODO: These should be not in epsilon.
 	//a.InEpsilon(rv.X, 0, 0.1)
 	//a.InEpsilon(rv.Y, -1, 0.1)
+}
+
+func BenchmarkRotateVector(b *testing.B) {
+	v := vector.Vector{X: 0, Y: 1}
+	r := Rotator{math.Pi}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		r.RotateVector(v)
+	}
 }

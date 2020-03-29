f85741548f8180b6abfa4ede594ff2eb1cb4b73c
Jaewan Park <jaewan.huey.park@gmail.com>
2020-03-29 23:11:40 +0900

    Create BenchmarkCross

---
+++ core/math/vector/vector_test.go
@@ -0,0 +1,13 @@
+package vector
+
+import "testing"
+
+func BenchmarkCross(b *testing.B) {
+	l := Vector{0, 0}
+	r := Vector{1, 0}
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		l.Cross(r)
+	}
+}

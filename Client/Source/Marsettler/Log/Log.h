f22468d0d626d7933a351ea7b1a2dc83fc104613
Jaewan Park <jaewan.huey.park@gmail.com>
2020-12-27 11:07:14 +0900

    로그 매크로 추가

---
+++ Client/Source/Marsettler/Log/Log.h
@@ -0,0 +1,7 @@
+﻿#pragma once
+
+#include "Marsettler.h"
+
+#define LOG_PRINT(s, ...) UE_LOG(LogPrint, Log, TEXT(s), ##__VA_ARGS__)
+
+#define LOG_FATAL(s, ...) UE_LOG(LogFatal, Fatal, TEXT(s), ##__VA_ARGS__)
